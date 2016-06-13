768333776925a61a9824cbadac33d06cb2977634
suneelvanapalli <suneel.vanapalli@gmail.com>
2016-06-13 20:12:52 +1000

    Adding initial VS MVC project template for Digi Cart Client application.

---
+++ DigiCartClient/DigiCartClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DigiCartClient.Startup))]
+namespace DigiCartClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
