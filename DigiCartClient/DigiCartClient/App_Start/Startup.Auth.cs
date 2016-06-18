c3ac0f0279bac16f44e6424fc85af6a31d74b724
suneelvanapalli <suneel.vanapalli@gmail.com>
2016-06-18 11:43:17 +1000

    a sample middleware using owin

--- DigiCartClient/DigiCartClient/App_Start/Startup.Auth.cs
+++ DigiCartClient/DigiCartClient/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using DigiCartClient.Models;
+using DigiCartClient.Middlewares;
 
 namespace DigiCartClient
 {
@@ -19,6 +20,7 @@ namespace DigiCartClient
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
+            app.Use<TeapotMiddleware>();
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             // Configure the sign in cookie
---
+++ DigiCartClient/DigiCartClient/Middlewares/WorkflowCheckMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DigiCartClient.Middlewares
+{
+    public class TeapotMiddleware : OwinMiddleware
+    {
+        public TeapotMiddleware(OwinMiddleware next) : base(next)
+        {
+        }
+
+        public async override Task Invoke(IOwinContext context)
+        {
+            context.Response.StatusCode = 418;
+           await this.Next.Invoke(context);
+        }
+    }
+}
